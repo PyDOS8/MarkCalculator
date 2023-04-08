8812a5d25c43066bf0643715af01c04288b760dc
PyTerminal <124309800+PyTerminal@users.noreply.github.com>
2023-04-08 14:34:59 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,12 +6,12 @@ int main(){
     cin >> amount;
     if(amount == 1){
         int right;
-        int wrong;
+        int questions;
         printf("How many questions were there? ");
-        cin >> wrong;
+        cin >> questions;
         printf("How many did you get right? ");
         cin >> right;
-        int calc = wrong/right;
+        int calc = questions/right;
         int ans = calc*100;
         cout << "The result is " << ceil(ans) << endl;
         cout << "Here's how you calculate it: 1000(questions correct)/1100(amount of questions)*100 =91%";
